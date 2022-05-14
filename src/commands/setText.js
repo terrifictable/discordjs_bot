7c8d8c3c1d6328ce3108defd1cad990e6c34ded1
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-14 15:02:08 +0200

    set what esp is supposed to display

---
+++ src/commands/setText.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { topic } = require("../resources/config.json");
+const { mqtt_init, publish } = require("../common/_mqtt");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName('settext')
+            .setDescription('Set Text for ESP to display')
+            .addStringOption(
+                option => option.setName('input')
+                    .setDescription('Text to be displayed on ESP')
+                    // .setRequired(true)
+            ),
+            
+    async execute(interaction) {
+        var client = mqtt_init();
+        publish(client, topic, interaction.options.getString('input'));
+
+        await interaction.reply("Sent message to MQTT");
+    },
+}
