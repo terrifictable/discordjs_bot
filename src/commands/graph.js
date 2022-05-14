75355f8e3c921c10f8f00592e9f01a884aa72a76
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-14 14:34:21 +0200

    reply with graph from grafana when executed (Grafana needs "Grafana Image Renderer" plugin to do this)

--- src/commands/graph.js
+++ src/commands/graph.js
@@ -1,6 +1,8 @@
 const { username, password } = require('../resources/config.json');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment } = require('discord.js');
 const fetch = require('node-fetch');
+const fse = require('fs-extra');
 const fs = require('node:fs');
 
 
@@ -11,23 +13,24 @@ module.exports = {
             
     async execute(interaction) {
         var currUnixTime            = new Date().getTime();
-        var graphStartUnixTime      = currUnixTime - 86400;
+        var graphStartUnixTime      = currUnixTime - 86400000;
         var url                     = `http://192.168.2.132:3000/render/d-solo/y-0Qde_nz/airgiano-sdc30?orgId=1&from=${graphStartUnixTime}&to=${currUnixTime}&refresh=5s&panelId=2&width=2000&height=600&tz=Europe%2FBerlin`
         var auth                    = "Basic " + new Buffer.alloc((username + ":" + password).length, username + ":" + password).toString("base64");
-    
-        // request.get({url : url, headers : { "Authorization" : auth } },
-        //         function (error, response) {
-        //             fs.writeFile("./image.png", response.body.Buffer(), () => { console.log("Image saved"); });
-        //         }
-        // );
-
+        
+        await interaction.deferReply();
+        // await interaction.editReply(`Grafana Graph from ${new Date(graphStartUnixTime*1000)} to ${new Date(currUnixTime*1000)}`);
 
         // TODO
-        const response = await fetch(url, { method: 'GET', headers: { "Authorization" : auth } });
-        const buffer = await response.buffer();
-        fs.writeFile(`./image.jpg`, buffer, () => { console.log('finished downloading!') });
+        try { fse.removeSync("./data/image.jpg"); }
+        catch (err) { /* console.log(err) */ }
 
-    
-        await interaction.reply("graph");
-    },
+        const response = await fetch(url, { method: 'GET', headers: { "Authorization" : auth } });
+        const buffer = await response.buffer();        
+        fs.writeFile(`./data/image.jpg`, buffer, () => { console.log('finished downloading!') });
+
+
+        const img = new MessageAttachment("./data/image.jpg");
+        
+        await interaction.editReply({ files: [img] }); 
+    }
 }
