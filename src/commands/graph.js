05c2e826c34efc1ab1d6e750e1aca3d80d3044f1
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-14 14:42:22 +0200

    get image of grafana graph (you may need to edit the image url for this to work)

--- src/commands/graph.js
+++ src/commands/graph.js
@@ -1,9 +1,12 @@
-const { username, password } = require('../resources/config.json');
+const { username, password, grafana } = require('../resources/config.json');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageAttachment } = require('discord.js');
 const fetch = require('node-fetch');
 const fse = require('fs-extra');
 const fs = require('node:fs');
+
+
+// !!! THIS REQUIRES "Grafana Image Renderer" PLUGIN TO BE INSTALLED ON Grafana SERVER !!!
 
 
 module.exports = {
@@ -14,23 +17,23 @@ module.exports = {
     async execute(interaction) {
         var currUnixTime            = new Date().getTime();
         var graphStartUnixTime      = currUnixTime - 86400000;
-        var url                     = `http://192.168.2.132:3000/render/d-solo/y-0Qde_nz/airgiano-sdc30?orgId=1&from=${graphStartUnixTime}&to=${currUnixTime}&refresh=5s&panelId=2&width=2000&height=600&tz=Europe%2FBerlin`
-        var auth                    = "Basic " + new Buffer.alloc((username + ":" + password).length, username + ":" + password).toString("base64");
+        var url                     = `http://${grafana}/render/d-solo/y-0Qde_nz/airgiano-sdc30?orgId=1&from=${graphStartUnixTime}&to=${currUnixTime}&refresh=5s&panelId=2&width=2000&height=600&tz=Europe%2FBerlin`
+        var auth                    = "Basic " + new Buffer.alloc((username + ":" + password).length, username + ":" + password).toString("base64"); // Grafana Username and Password authentication
         
-        await interaction.deferReply();
-        // await interaction.editReply(`Grafana Graph from ${new Date(graphStartUnixTime*1000)} to ${new Date(currUnixTime*1000)}`);
+        await interaction.deferReply(); // wait until the reply is edited, for interaction token something to not be invalid, incase this takes longer than 3 secs
 
-        // TODO
-        try { fse.removeSync("./data/image.jpg"); }
-        catch (err) { /* console.log(err) */ }
-
-        const response = await fetch(url, { method: 'GET', headers: { "Authorization" : auth } });
-        const buffer = await response.buffer();        
-        fs.writeFile(`./data/image.jpg`, buffer, () => { console.log('finished downloading!') });
-
-
-        const img = new MessageAttachment("./data/image.jpg");
         
-        await interaction.editReply({ files: [img] }); 
+        try { fse.removeSync("./data/image.jpg"); } // Remove old image
+        catch (err) { "" } // catch (incase there is no image)
+
+
+        const response = await fetch(url, { method: 'GET', headers: { "Authorization" : auth } }); // send request to grafana
+        const buffer = await response.buffer(); // get image
+        fs.writeFile(`./data/image.jpg`, buffer, () => { console.log("Grafana Graph saved") }); // save image
+
+
+        const img = new MessageAttachment("./data/image.jpg"); // create a new attachment
+        
+        await interaction.editReply({ files: [img] }); // send attachment
     }
 }
