4d64a57b8c70f61610a3c6da8659a79f89f6184b
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-14 11:55:20 +0200

    added command and event "imports" from diffrent files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-const discordjs = require('discordjs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./resources/config.json');
+const fs = require('node:fs');
+
+client.commands = new Collection();
+const client = new Client({ intents: Intents.FLAGS.GUILDS });
+
+
+
+// LOAD EVENTS AND COMMANDS
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+
+
+
+
+
+client.login(token);
