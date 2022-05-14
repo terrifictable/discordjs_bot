867e95de235a0f847083483d324ef836452047b2
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-14 12:00:29 +0200

    actually do something if command is typed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const client = new Client({ intents: Intents.FLAGS.GUILDS });
 const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
-
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
@@ -20,13 +19,27 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
 
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+
+});
 
 
 
